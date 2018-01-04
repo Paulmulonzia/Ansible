4a9e28d8b7d2768bd852ea0d409706dbffeb794b
Paulmulonzia <paulmwendwa43@gmail.com>
2018-01-04 14:31:02 +0300

    First commit

---
+++ OverallDspace.sh
@@ -0,0 +1,8 @@
+ansible-playbook /etc/Dspace\ Dependencies.yml
+ansible-playbook /etc/Add user.yml
+ansible-playbook /etc/Dspace1.yml
+ansible-playbook /etc/Dspace2.yml
+ansible-playbook /etc/Dspace3.yml
+ansible-playbook /etc/Dspace4.yml
+ansible-playbook /etc/Dspace5.yml
+
---
+++ OverallTomcat.sh
@@ -0,0 +1,6 @@
+ansible-playbook /etc/tomcat\ user\ and\ group.yml
+ansible-playbook /etc/tomcat1.yml
+ansible-playbook /etc/tomcat2.yml
+ansible-playbook /etc/tomcat3.yml
+ansible-playbook /etc/tomcat4.yml
+ansible-playbook /etc/tomcat5.yml
