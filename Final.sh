daac79efcf84c256ee57839d76f11f167c8780e6
Paulmulonzia <paulmwendwa43@gmail.com>
2018-01-04 14:12:12 +0300

    First commit

--- Final.sh
+++ Final.sh
@@ -1,3 +1,5 @@
 ansible-playbook /etc/ansible/OverallTomcat.yml
 ansible-playbook /etc/ansible/OverallDspace.yml
 ansible-playbook /etc/ansible/Apache.yml
+ansible-playbook /etc/ansible/ApacheTomcatDependencies.yml
+
